4cd5cdd7cb4f674c412dbcd6fead1c4d93a08636
Namrata Rajkumar Motwani <namratamotwani@Namratas-MacBook-Pro.local>
2018-09-18 11:07:38 +0530

    Selenium test2

--- test2.js
+++ test2.js
@@ -10,6 +10,7 @@ var capabilities = {
 
 var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(capabilities).build();
 driver.get('https://the-internet.herokuapp.com/javascript_alerts');
+driver.getCurrentUrl();
 
 // Click 'Click for JS Alert'.
 driver.findElement(By.xpath("//*[contains(text(),'Click for JS Alert')]")).click();
@@ -18,14 +19,25 @@ driver.switchTo().alert().accept();
 // Click 'Click for JS Confirm'. Clicks 'OK'. Assert the Result value 'You clicked: Ok'
 driver.findElement(By.xpath("//*[contains(text(),'Click for JS Confirm')]")).click();
 driver.switchTo().alert().accept();
-var a=driver.findElement(By.id("result")).getText();
-assert.equal("You clicked: Ok",a.value_,"error1");
+driver.findElement(By.id("result")).getText().then(function (ans1){
+	if(assert.equal("You clicked: Ok",ans1,"error1")=== undefined)
+	{
+		console.log("Correct value 1");
+	}
+
+});
+
 
 // Click 'Click for JS Prompt'. Enter 'BrowserStack' in the prompt. Assert the Result value 'You entered: BrowserStack' Screen reader support enabled.
 driver.findElement(By.xpath("//*[contains(text(),'Click for JS Prompt')]")).click();
 driver.switchTo().alert().sendKeys("BrowserStack");
 driver.switchTo().alert().accept();
-var b=driver.findElement(By.id("result")).getText();
-assert.equal("You entered: BrowserStack",b.value_,"error2");
+driver.findElement(By.id("result")).getText().then(function (ans2){
+	if(assert.equal("You entered: BrowserStack",ans2,"error2")=== undefined)
+	{
+		console.log("Correct value 2");
+	}
+
+});
 
 driver.quit();
